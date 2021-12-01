9dffed5ef92f1f023baa3e48d38c26beb1118dc4
Tomas Tauer <tomas.tauer@pipedrive.com>
2021-12-01 06:09:44 +0100

    Solution day 1

--- src/solutions/day01/index.ts
+++ src/solutions/day01/index.ts
@@ -2,10 +2,14 @@ import { Solution } from '../../utilities/solver.ts';
 
 export default class Day01 implements Solution {
 	solvePart1(input: string[]) {
-		return 0;
+		return input.map((i) => parseInt(i)).filter((c, i, a) => i > 0 && c - a[i - 1] > 0).length;
 	}
 
 	solvePart2(input: string[]) {
-		return 0;
+		return input
+			.map((i) => parseInt(i))
+			.map((c, i, a) => (i < 2 ? 0 : c + a[i - 1] + a[i - 2]))
+			.slice(2)
+			.filter((c, i, a) => i > 0 && c - a[i - 1] > 0).length;
 	}
 }
