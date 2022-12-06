42b61575ea38507ec24a29df7ff69d1ad8267271
Tomas Tauer <tomas.tauer@pipedrive.com>
2022-12-06 07:57:57 +0100

    Day 6

---
+++ src/solutions/day06/index.ts
@@ -0,0 +1,35 @@
+import { Solution } from '../../utilities/solver.ts';
+
+export default class Day01 implements Solution {
+	solvePart1(input: string[]) {
+		const window: string[] = [];
+
+		for(let i = 0; i < input[0].length; i++) {
+			if(window.length === 4) {
+				window.shift();
+			}
+			window.push(input[0][i]);
+
+			if(new Set(window).size === 4) {
+				return i+1;
+			}
+		}
+		return 0;
+	}
+
+	solvePart2(input: string[]) {
+		const window: string[] = [];
+
+		for(let i = 0; i < input[0].length; i++) {
+			if(window.length === 14) {
+				window.shift();
+			}
+			window.push(input[0][i]);
+
+			if(new Set(window).size === 14) {
+				return i+1;
+			}
+		}
+		return 0;
+	}
+}
